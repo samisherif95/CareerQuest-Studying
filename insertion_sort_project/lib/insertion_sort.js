de4da8123e5ee5d56a61b21c4ca531126deb5418
Sami Ellaboudy <sami.sherif95@gmail.com>
2020-02-20 18:06:39 -0800

    sorting algo

--- insertion_sort_project/lib/insertion_sort.js
+++ insertion_sort_project/lib/insertion_sort.js
@@ -1,5 +1,12 @@
 function insertionSort(arr) {
-
+    for(let i = 0;i < arr.length ; i++){
+        let curr = arr[i]
+        for(var j = i-1; j >=0 && curr < arr[j]; j--){
+            arr[j+1] = arr[j]
+        }
+        arr[j+1] = curr
+    }
+    return arr
 }
 
 module.exports = {
